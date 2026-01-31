b9529752ca873d5fdbdb6ccf9a86e16ab73ac486
mdukle <mdukle@uiowa.edu>
2026-01-31 13:45:25 -0600

    part 1 of assignment and set up of part 2

---
+++ echo.py
@@ -0,0 +1,15 @@
+#echo.py
+
+def echo(text: str, repetitions: int =3) -> str:
+    """Imitate a real-world echo."""
+    if not text:
+        print("Please input characters.")
+    else:
+        for i in range(3, 0, -1):
+            print(text[-i:])
+        print(".")
+
+
+if __name__ == "__main__":
+    text = input("Yell something at a mountain: ")
+    print(echo(text))
